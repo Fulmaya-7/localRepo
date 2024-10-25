da407cc3c3b58bae582a6d93de843b080bcfc180
Fulmaya tamang <fmaya856@gmail.com>
2024-10-25 21:07:55 +0545

    Updated fulu.java with code

---
+++ fulu.java
@@ -0,0 +1,10 @@
+package LocalRepo;
+
+import java.util.*;
+public class fulu {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
